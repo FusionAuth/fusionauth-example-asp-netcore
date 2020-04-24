18e5469896f6f11193c6003dc5ac8f9fe6f14929
Dan Moore <bitbucket@mooreds.com>
2020-04-24 12:13:34 -0600

    Added page to be secured.
    Including to the top menu.

---
+++ SampleApp/Pages/Secure.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace SampleApp.Pages
+{
+    public class SecureModel : PageModel
+    {
+        private readonly ILogger<SecureModel> _logger;
+
+        public SecureModel(ILogger<SecureModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
