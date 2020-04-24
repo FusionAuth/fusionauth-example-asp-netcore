3c481e4d47d4f8f022e3d720d85d0094134d185a
Dan Moore <bitbucket@mooreds.com>
2020-04-24 15:43:46 -0600

    Make sure we log out of the identity server.
    We need to grab the configuration for the identity server and log out
    there as well. Pulling that from the config.

--- SampleApp/Pages/Logout.cshtml.cs
+++ SampleApp/Pages/Logout.cshtml.cs
@@ -6,23 +6,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System.Security.Claims;
+using Microsoft.Extensions.Configuration;
 
 namespace SampleApp.Pages
 {
     public class LogoutModel : PageModel
     {
         private readonly ILogger<LogoutModel> _logger;
+	private readonly IConfiguration _configuration;
 
-        public LogoutModel(ILogger<LogoutModel> logger)
+        public LogoutModel(ILogger<LogoutModel> logger, IConfiguration configuration)
         {
             _logger = logger;
+	    _configuration = configuration;
         }
 
 	public IActionResult OnGet()
 	{
               SignOut("cookie", "oidc");
+              var host = _configuration["SampleApp:Authority"];
+
+              var clientId = _configuration["SampleApp:ClientId"];
+              var url = host + "/oauth2/logout?client_id="+clientId;
               Response.Cookies.Delete("mycookie");
-	      return Redirect("/");
+              return Redirect(url);
         }
     }
 }
