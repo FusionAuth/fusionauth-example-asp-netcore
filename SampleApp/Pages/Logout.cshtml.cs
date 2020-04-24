bb592f3a97437860b255713a69ac3c41057c0b71
Dan Moore <bitbucket@mooreds.com>
2020-04-24 15:15:57 -0600

    Added logout.
    The SignOut method doesn't seem to remove the session cookies, so I remove
    it in my code as well.

---
+++ SampleApp/Pages/Logout.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+
+namespace SampleApp.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(ILogger<LogoutModel> logger)
+        {
+            _logger = logger;
+        }
+
+	public IActionResult OnGet()
+	{
+              SignOut("cookie", "oidc");
+              Response.Cookies.Delete("mycookie");
+	      return Redirect("/");
+        }
+    }
+}
